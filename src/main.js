2222e12d68ab608fd715f04f62dadafe49940d79
YJack <jackleagends@gmail.com>
2021-12-21 18:58:34 +0800

    毒瘤寫法 之後可能要改 --------------------------- 使用 onAuthStateChanged，在 authentication status 的狀態沒有改變之前不 create app 來達成 app 每次 create 時候已經認證過

--- src/main.js
+++ src/main.js
@@ -1,27 +1,36 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import installElementPlus from './plugins/element'
-import './element-variables.scss'
-import router from './router'
-import * as firebase from "firebase/app";
 
-/* code from our Firebase console */
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import router from './router'
+
+import { initializeApp } from 'firebase/app'
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAwMK5t4rvCI_WadLR9egNgSlpOdMDREDU",
-  authDomain: "sad-project-4089f.firebaseapp.com",
-  projectId: "sad-project-4089f",
-  storageBucket: "sad-project-4089f.appspot.com",
-  messagingSenderId: "878782521598",
-  appId: "1:878782521598:web:6f1ab3a5db5f9a81fb85af",
-  measurementId: "G-1DJE4Z1X3K"
+    apiKey: "AIzaSyAwMK5t4rvCI_WadLR9egNgSlpOdMDREDU",
+    authDomain: "sad-project-4089f.firebaseapp.com",
+    projectId: "sad-project-4089f",
+    storageBucket: "sad-project-4089f.appspot.com",
+    messagingSenderId: "878782521598",
+    appId: "1:878782521598:web:6f1ab3a5db5f9a81fb85af",
+    measurementId: "G-1DJE4Z1X3K"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig)
 
-const app = createApp(App)
+let app
 
-app.use(router)
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+onAuthStateChanged(getAuth(), (_user) => {
+    if (!app) { // logged in
+        app = createApp(App)
+        app.config.globalProperties.firebase_user = _user
+        app.use(router).use(ElementPlus).mount('#app')
+    }else if(_user != null){
+        app.config.globalProperties.firebase_user = _user
+    }
+})
 
-installElementPlus(app)
-app.mount('#app')
+export default { firebaseApp }
