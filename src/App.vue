1452db344cb7f6ca5f6d6769fccbcef505651fc7
YJack <jackleagends@gmail.com>
2021-12-21 18:54:43 +0800

    純做 router

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,11 @@
   <router-view />
 </template>
 
-<script setup>
-  import { ref } from 'vue' // used for conditional rendering
-  import { getAuth, onAuthStateChanged } from "firebase/auth";
-  //import { useRouter } from 'vue-router'
-  //const router = useRouter()
-  const isLoggedIn = ref(true)
-
-  const auth = getAuth();
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      const uid = user.uid;
-      console.log(uid)
-      isLoggedIn.value = true // if we have a user
-    } else {
-      // User is signed out
-      isLoggedIn.value = false // if we do not
-    }
-  });
-
-  /*const logOut = () => {
-    signOut(auth).then(() => {
-      router.push("/")
-    }).catch((error) => {
-      console.log(error)
-    });
-  }*/
-
+<script>
 </script>
 
 <style>
-  body {margin:0;padding:0}
+  body {margin:0;padding:0;height: 100%; background-color: rgb(250, 250, 250);}
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
