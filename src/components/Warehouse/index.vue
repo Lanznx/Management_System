ef206c0687b846a18442ecb205c83d6d763fa803
YJack <jackleagends@gmail.com>
2021-12-23 00:30:16 +0800

    倉儲管理頁面 Index 含導航欄

---
+++ src/components/Warehouse/index.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <el-breadcrumb separator="/" style="padding: 10px">
+        <el-breadcrumb-item :to="{ path: '/sys/ware-house' }">倉庫管理</el-breadcrumb-item>
+        <el-breadcrumb-item v-if="routePath != '/sys/ware-house/menu'"> 倉庫 ID: {{ routeId }}</el-breadcrumb-item>
+    </el-breadcrumb>
+    <router-view />
+</template>
+
+<script>
+import { useRoute } from 'vue-router'
+export default{
+    data(){
+        return {}
+    },
+    created(){
+        console.log(this.routePath)
+    },
+    computed:{
+        routePath: function(){
+            return useRoute().path
+        },
+        routeId: function(){
+            return useRoute().params.id
+        }
+    }
+}
+</script>
