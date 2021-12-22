30d2644891ddb5ff9f606b16362d72778c15dd6e
YJack <jackleagends@gmail.com>
2021-12-22 23:00:07 +0800

    Warehouse 頁面可以顯示圖片

--- src/components/Warehouse/Warehouse.vue
+++ src/components/Warehouse/Warehouse.vue
@@ -1,23 +1,23 @@
 <template>
     <el-breadcrumb separator=">" style="padding: 10px">
-        <el-breadcrumb-item :to="{ path: '/' }">asdasd</el-breadcrumb-item>
+        <el-breadcrumb-item :to="{ path: '/' }">asd</el-breadcrumb-item>
     </el-breadcrumb>
-    <el-row>
+    <el-row v-if="inited">
+        <span> {{ warehouseData.length }} </span>
         <el-col
-        v-for="(o, index) in 2"
-        :key="o"
+        v-for="(warehouse, index) in warehouseData"
+        :key="index"
         :span="8"
-        :offset="index > 0 ? 2 : 0"
         >
         <el-card :body-style="{ padding: '0px' }">
             <img
-            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+            :src="warehouse.photo"
             class="image"
             />
             <div style="padding: 14px">
-            <span>Yummy hamburger</span>
+            <span> {{ warehouse.name }} </span>
             <div class="bottom">
-                <time class="time">{{ currentDate }}</time>
+                <time class="time">{{ warehouse.location }}</time>
                 <el-button type="text" class="button">Operating</el-button>
             </div>
             </div>
@@ -27,22 +27,52 @@
 </template>
 
 <script>
-import { ref } from 'vue'
-//import { useRoute } from 'vue-router'
+import {getFirestore, getDocs, collection, query, where } from 'firebase/firestore'
 
-export default{
-    components:{
-    },
-    setup() {
-        const currentDate = ref(new Date())
-
-        return {
-            currentDate,
-        }
-    },
-    created(){
-        console.log(this.firebase_user)
+export default {
+  data() {
+    return {
+      inited: false,
+      warehouseData: [
+        // {
+        //     location: "116台北市文山區指南路三段40巷6號",
+        //     name: "貓空倉庫",
+        //     owner: "UnKxh75XreckUTf1ef1FVp0XV963",
+        //     photo: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cargowise.com%2Fzh-hant%2Fsolutions%2Fcargowise-warehouse%2F&psig=AOvVaw0V6QARBXRWreb2AJ5TxDUN&ust=1640270970185000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOjFrtbT9_QCFQAAAAAdAAAAABAD"
+        // }
+      ]
     }
+  },
+
+  methods: {
+    async buildCard(uid) {
+        this.warehouseData = []
+
+        const db = getFirestore()
+
+        const citiesRef = collection(db, "warehouse");
+        const q = query(citiesRef, where("owner", "==", uid));
+
+        console.log("getting docs from warehouse/" + "owner" + "==" + uid)
+        const querySnapshot = await getDocs(q)
+        // intitailize tableData
+        await querySnapshot.forEach((doc) => {
+            // console.log(`${doc.id} => ${doc.data()}`)
+            this.warehouseData.push(doc.data())
+        });
+
+        console.log(this.warehouseData)
+
+        this.inited = true
+    },
+    handleDetail(name){
+        console.log("Goto " + name)
+    }
+  },
+  created(){
+      console.log(this.firebase_user.uid)
+      this.buildCard(this.firebase_user.uid)
+  }
 }
 </script>
 
