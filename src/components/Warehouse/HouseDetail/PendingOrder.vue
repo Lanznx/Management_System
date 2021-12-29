c363edabf832cfebe67123eb48455a8922537204
YJack <jackleagends@gmail.com>
2021-12-29 23:04:59 +0800

    approve 前先確認 和 時時監聽數據

--- src/components/Warehouse/HouseDetail/PendingOrder.vue
+++ src/components/Warehouse/HouseDetail/PendingOrder.vue
@@ -1,21 +1,39 @@
 <template>
+    <el-alert v-if="op_success" title="success alert" type="success" center show-icon> </el-alert>
   <el-table :data="list" :height="tableHeight" :default-sort="{ prop: 'date', order: 'descending' }" style="width: 100%;">
     <el-table-column fixed prop="date" width="150" sortable label="Date"></el-table-column>
     <el-table-column prop="id" label="Order_ID" align="center"></el-table-column>
     <el-table-column prop="operation" label="Operation" width="100"></el-table-column>
     <el-table-column label="Approve" fixed="right" width="150" align="center">
         <template #default="scope">
-            <el-button type="success" circle @click="handleApprove(scope.row)"><el-icon><Check /></el-icon></el-button>
+            <el-popconfirm 
+            title="Are you sure to approve this?"
+            @confirm="handleApprove(scope.row)"
+            >
+                <template #reference>
+                    <el-button type="success" circle><el-icon><Check /></el-icon></el-button>
+                </template>
+            </el-popconfirm>
         </template>
     </el-table-column>
   </el-table>
 </template>
 
 <script>
-import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore'
+import { getFirestore, collection, query, where, 
+getDocs, 
+doc, 
+//getDoc, 
+onSnapshot,
+setDoc,
+//limit,
+orderBy 
+} from 'firebase/firestore'
 import {
   Check
 } from '@element-plus/icons'
+import { ElMessage } from 'element-plus'
+import { useRoute } from 'vue-router'
 
 export default {
     components: {
@@ -24,44 +42,129 @@ export default {
     props:{
         tableHeight: Number
     },
+    computed: {
+        routeId: function(){
+            return useRoute().params.id
+        }
+    },
     data() {
         return {
-        list: null
+            list: null,
+            op_success: false
         }
     },
     created() {
-        this.fetchData(this.firebase_user.uid)
+        this.fetchData(this.routeId)
     },
     methods: {
-        async fetchData(uid) {
-        this.list = []
+        async fetchData(id) {
+            console.log(id)
 
-        const db = getFirestore()
+            const db = getFirestore()
 
-        const ordersRef = collection(db, "orders");
-        const q = query(ordersRef, where("user", "==", uid), where("status", "==", "Pending"));
+            const ordersRef = collection(db, "orders");
+            const q = query(ordersRef, where("to", "==", id), where("status", "==", "Pending"), orderBy("date", "desc"));
 
-        console.log("getting docs from orders/" + "user" + "==" + uid)
-        const querySnapshot = await getDocs(q)
-        // intitailize tableData
-        await querySnapshot.forEach((doc) => {
-            // console.log(`${doc.id} => ${doc.data()}`)
-            console.log(doc.data().date.toDate().toISOString().substring(0, 10))
+            console.log("getting docs from orders/" + "to" + "==" + id)
+            onSnapshot(q, (snapshot) => {
+                this.list = []
+                snapshot.forEach((doc) => {
+                    // console.log(`${doc.id} => ${doc.data()}`)
+                    console.log(doc.data().date.toDate().toISOString().substring(0, 10))
 
-            this.list.push({
-                id: doc.id,
-                user: doc.data().user,
-                amount: doc.data().amount,
-                date: doc.data().date.toDate().toISOString().substring(0, 10),
-                from: doc.data().from,
-                operation: doc.data().operation,
-                product: doc.data().product,
-                status: doc.data().status,
-                to: doc.data().to
+                    this.list.push({
+                        id: doc.id,
+                        user: doc.data().user,
+                        amount: doc.data().amount,
+                        date: doc.data().date.toDate().toISOString().substring(0, 10),
+                        from: doc.data().from,
+                        operation: doc.data().operation,
+                        product: doc.data().product,
+                        status: doc.data().status,
+                        to: doc.data().to
+                    })
+                });
+                console.log(this.list)
             })
-        });
 
-        console.log(this.list)
+        },
+        handleApprove(row){
+            console.log(row)
+            
+            this.approve(row)
+
+            ElMessage({
+                message: '同意 ' + row.id,
+                type: 'success',
+            })
+        },
+        async approve(order){
+            const id = order.id
+
+            // 設定減少者跟增加者
+            let purchaser, seller
+            if(order.operation == "Sell"){
+                seller = order.from
+                purchaser = order.to
+            }else if(order.operation == "Purchase"){
+                purchaser = order.from
+                seller = order.to
+            }else{
+                console.log("invaild operation")
+                return ;
+            }
+
+            const product = order.product
+            console.log(purchaser + " buy " + product + " from " + seller)
+
+            const db = getFirestore()
+            // seller 東西減少
+            let getDocRef = collection(db, "warehouse/" + seller + "/inventory");
+            const sellerQuery = query(getDocRef, where("serial_num", "==", product))
+            let docSnap = await getDocs(sellerQuery);
+            console.log(docSnap)
+
+            let newSelledProduct, newSelledProductId
+            docSnap.forEach((doc)=>{
+                newSelledProduct = doc.data()
+                newSelledProductId = doc.id
+            })
+            newSelledProduct.amount -= order.amount
+
+            console.log(newSelledProduct)
+            
+            await setDoc(doc(db, "warehouse/" + seller + "/inventory", newSelledProductId), newSelledProduct);
+            console.log("seller -");
+
+            // buyer 東西增加
+            getDocRef = collection(db, "warehouse/" + purchaser + "/inventory");
+            const purchaserQuery = query(getDocRef, where("serial_num", "==", product))
+            docSnap = await getDocs(purchaserQuery);
+            console.log(docSnap)
+
+            let newPurchasedProduct, newPurchasedProductId
+            docSnap.forEach((doc)=>{
+                newPurchasedProduct = doc.data()
+                newPurchasedProductId = doc.id
+            })
+
+            if(newPurchasedProduct == null){
+                newPurchasedProductId = newSelledProductId
+                newPurchasedProduct = newSelledProduct
+                newPurchasedProduct.amount = order.amount
+            }else 
+                newPurchasedProduct.amount += order.amount
+
+            console.log(newPurchasedProduct)
+            
+            await setDoc(doc(db, "warehouse/" + purchaser + "/inventory", newPurchasedProductId), newPurchasedProduct);
+            console.log("purchaser +");
+
+
+            // 將狀態改為 Success
+            const orderRef = doc(db, 'orders', id);
+            await setDoc(orderRef, { status: "Success" }, { merge: true });
+            console.log("set doc success")
         }
     }
 }
