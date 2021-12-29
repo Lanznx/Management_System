4a68e94d81bfa0922c3c76876461940f38ec3123
YJack <jackleagends@gmail.com>
2021-12-29 23:04:34 +0800

    變更兩個 table 佔比

--- src/components/Warehouse/HouseDetail/House.vue
+++ src/components/Warehouse/HouseDetail/House.vue
@@ -1,19 +1,20 @@
 <template>
     <el-row :gutter="20">
-        <el-col :span="14">
+        <el-col :span="12">
+        <el-divider content-position="left">倉儲</el-divider>
             <el-table :data="warehouseData" style="width: 100%">
                 <el-table-column type="expand">
                     <template #default="props">
                         <InventoryDetail :inv_id="props.row.serial_num"/>
                     </template>
                 </el-table-column>
-                <el-table-column label="Serial_Num" width="175" prop="serial_num" />
+                <el-table-column label="Serial_Num" width="205" prop="serial_num" />
                 <el-table-column label="名稱" prop="name" />
-                <el-table-column label="數量" width="105" prop="amount" />
-                <el-table-column label="價值" width="105" prop="value" />
+                <el-table-column label="數量" width="155" prop="amount" />
+                <!--<el-table-column label="價值" width="105" prop="value" />-->
             </el-table>
         </el-col>
-        <el-col :span="10">
+        <el-col :span="12">
             <el-divider content-position="left">待審核</el-divider>
             <el-row>
             <PendingOrder :tableHeight="350" />
@@ -38,7 +39,10 @@ getFirestore,
 //doc,
 //deleteDoc,
 collection,
-getDocs } from 'firebase/firestore'
+//getDocs,
+query,
+limit,
+onSnapshot} from 'firebase/firestore'
 
 export default {
     components: {
@@ -57,19 +61,20 @@ export default {
     },
     methods: {
         async createWarehouseTable(ware_id){
-            this.warehouseData = []
-
             const db = getFirestore()
 
             console.log("getting docs from warehouse/" + ware_id + "/inventory")
-            const querySnapshot = await getDocs(collection(db, "warehouse/" + ware_id + "/inventory"))
-            // intitailize tableData
-            await querySnapshot.forEach((doc) => {
-                // console.log(`${doc.id} => ${doc.data()}`)
-                this.warehouseData.push(
-                    doc.data()
-                )
-            });
+            const q = query(collection(db, "warehouse/" + ware_id + "/inventory"), limit(10))
+            
+            onSnapshot(q, (snapshot)=>{
+                this.warehouseData = []
+                snapshot.forEach((doc) => {
+                    // console.log(`${doc.id} => ${doc.data()}`)
+                    this.warehouseData.push(
+                        doc.data()
+                    )
+                });
+            })
         }
     },
     data(){
