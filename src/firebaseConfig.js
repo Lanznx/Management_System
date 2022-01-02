71104a21e8e02d59415b8d15031ebeea339c268d
YJack <jackleagends@gmail.com>
2022-01-02 20:26:52 +0800

    不給看 firebaseConfig

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwMK5t4rvCI_WadLR9egNgSlpOdMDREDU",
+    authDomain: "sad-project-4089f.firebaseapp.com",
+    projectId: "sad-project-4089f",
+    storageBucket: "sad-project-4089f.appspot.com",
+    messagingSenderId: "878782521598",
+    appId: "1:878782521598:web:6f1ab3a5db5f9a81fb85af",
+    measurementId: "G-1DJE4Z1X3K"
+};
+
+export { firebaseConfig }
