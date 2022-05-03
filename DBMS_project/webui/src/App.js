f1bd313de505dcc78e694fd775db323c49d2ba0e
YJack0000 <jackleagends@gmail.com>
2022-05-04 00:21:36 +0800

    更新基本 Intro 結構 * 這個看不懂直接跟我討論哈哈哈

--- DBMS_project/webui/src/App.js
+++ DBMS_project/webui/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import * as React from "react";
 import './App.css';
+
+import GlobalStyles from '@mui/material/GlobalStyles';
+
+import Intro from './Intro'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyles styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }} />
+      <Intro />
     </div>
   );
 }
