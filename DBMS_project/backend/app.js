d540fbbda17d9f0c56844949c2c93baa63be9312
grace0950 <pinyan0950@gmail.com>
2022-05-10 14:14:09 +0800

    [routes] -> add product & material

--- DBMS_project/backend/app.js
+++ DBMS_project/backend/app.js
@@ -8,6 +8,8 @@ const { mysqlPoolQuery } = require("./src/mysql");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var materialRouter = require("./routes/material");
+var productRouter = require("./routes/product");
 
 var app = express();
 
@@ -28,6 +30,8 @@ app.use(function (req, res, next) {
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/material", materialRouter);
+app.use("/product", productRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
