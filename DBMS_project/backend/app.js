f0887d6136d2c65d22037415307ce516bda3c281
grace0950 <pinyan0950@gmail.com>
2022-05-14 03:10:38 +0800

    [package] -> add module: dotenv

--- DBMS_project/backend/app.js
+++ DBMS_project/backend/app.js
@@ -5,6 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
 
+require("dotenv").config();
+
 const { mysqlPoolQuery } = require("./src/mysql");
 
 var indexRouter = require("./routes/index");
