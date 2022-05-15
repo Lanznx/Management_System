1c5cf0e1456fb3e0abf3817e4c982f146577031d
grace0950 <pinyan0950@gmail.com>
2022-05-16 00:06:07 +0800

    [material] -> add checkExisted

--- DBMS_project/backend/routes/material.js
+++ DBMS_project/backend/routes/material.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 const { v4: uuidv4 } = require("uuid");
 
+const { checkUserId, checkMaterialId } = require("../src/checkExisted");
+
 /* GET material listening. */
 router.get("/", function (req, res, next) {
   // #swagger.tags = ['Material']
@@ -18,35 +20,31 @@ router.post("/getAllMaterials", function (req, res, next) {
   */
   const mysqlPoolQuery = req.pool;
   const userId = req.body.userId;
-  mysqlPoolQuery(
-    "SELECT * FROM user WHERE user_id = ?",
-    userId,
-    function (err, rows) {
-      if (err) {
-        res.status(404).json({ success: false, err: err });
-      } else if (rows.length == 0) {
-        res.status(409).json({ success: false, err: "使用者不存在" });
-      } else {
-        mysqlPoolQuery(
-          "SELECT material_id AS materialId, material_name AS name, material_amount AS amount FROM material WHERE user_id = ?",
-          userId,
-          function (err, rows) {
-            if (err) {
-              res.status(404).json({ success: false, err: err });
+  checkUserId(userId, function (err, result) {
+    if (err) {
+      res.status(404).json({ success: false, err: err });
+    } else if (result == false) {
+      res.status(409).json({ success: false, err: "使用者不存在" });
+    } else {
+      mysqlPoolQuery(
+        "SELECT material_id AS materialId, material_name AS name, material_price AS price, material_amount AS amount FROM material WHERE user_id = ?",
+        userId,
+        function (err, rows) {
+          if (err) {
+            res.status(404).json({ success: false, err: err });
+          } else {
+            if (rows.length == 0) {
+              res.status(409).json({ success: false, err: "尚無原料" });
             } else {
-              if (rows.length == 0) {
-                res.status(409).json({ success: false, err: "尚無原料" });
-              } else {
-                res
-                  .status(200)
-                  .json({ success: true, allMaterialInformation: rows });
-              }
+              res
+                .status(200)
+                .json({ success: true, allMaterialInformation: rows });
             }
           }
-        );
-      }
+        }
+      );
     }
-  );
+  });
 });
 
 router.post("/getMaterialHistory", function (req, res, next) {
@@ -62,43 +60,35 @@ router.post("/getMaterialHistory", function (req, res, next) {
   const mysqlPoolQuery = req.pool;
   const userId = req.body.userId;
   const materialId = req.body.materialId;
-  mysqlPoolQuery(
-    "SELECT * from user WHERE user_id = ?",
-    userId,
-    function (err, rows) {
-      if (err) {
-        res.status(404).json({ success: false, err: err });
-      } else if (rows.length > 0) {
-        mysqlPoolQuery(
-          "SELECT * FROM material WHERE material_id = ?",
-          materialId,
-          function (err, rows) {
-            if (err) {
-              res.status(404).json({ success: false, err: err });
-            } else if (rows.length > 0) {
-              mysqlPoolQuery(
-                "SELECT price, amount, cost, time FROM material_history WHERE user_id = ? AND material_id = ?",
-                [userId, materialId],
-                function (err, rows) {
-                  if (err) {
-                    res.status(404).json({ success: false, err: err });
-                  } else {
-                    res
-                      .status(200)
-                      .json({ success: true, materialInformation: rows });
-                  }
-                }
-              );
-            } else {
-              res.status(409).json({ success: false, err: "原料不存在" });
+  checkUserId(userId, function (err, result) {
+    if (err) {
+      res.status(404).json({ success: false, err: err });
+    } else if (result == false) {
+      res.status(409).json({ success: false, err: "使用者不存在" });
+    } else {
+      checkMaterialId(materialId, function (err, result) {
+        if (err) {
+          res.status(404).json({ success: false, err: err });
+        } else if (result == false) {
+          res.status(409).json({ success: false, err: "原料不存在" });
+        } else {
+          mysqlPoolQuery(
+            "SELECT price, amount, cost, time FROM material_history WHERE user_id = ? AND material_id = ?",
+            [userId, materialId],
+            function (err, rows) {
+              if (err) {
+                res.status(404).json({ success: false, err: err });
+              } else {
+                res
+                  .status(200)
+                  .json({ success: true, materialInformation: rows });
+              }
             }
-          }
-        );
-      } else {
-        res.status(409).json({ success: false, err: "使用者不存在" });
-      }
+          );
+        }
+      });
     }
-  );
+  });
 });
 
 // 取得原料name與id的dictionary
@@ -114,35 +104,29 @@ router.post("/getMaterialDict", function (req, res, next) {
   */
   const mysqlPoolQuery = req.pool;
   const userId = req.body.userId;
-  mysqlPoolQuery(
-    "SELECT * FROM user WHERE user_id = ?",
-    userId,
-    function (err, rows) {
-      if (err) {
-        res.status(404).json({ success: false, err: err });
-      } else if (rows.length > 0) {
-        mysqlPoolQuery(
-          "SELECT material_name, material_id FROM material WHERE user_id = ? ",
-          userId,
-          function (err, rows) {
-            if (err) {
-              res.status(404).json({ success: false, err: err });
-            } else {
-              let materialDict = {};
-              for (let i = 0; i < rows.length; i++) {
-                materialDict[rows[i].material_name] = rows[i].material_id;
-              }
-              res
-                .status(200)
-                .json({ success: true, materialDict: materialDict });
+  checkUserId(userId, function (err, result) {
+    if (err) {
+      res.status(404).json({ success: false, err: err });
+    } else if (result == false) {
+      res.status(409).json({ success: false, err: "使用者不存在" });
+    } else {
+      mysqlPoolQuery(
+        "SELECT material_name, material_id FROM material WHERE user_id = ? ",
+        userId,
+        function (err, rows) {
+          if (err) {
+            res.status(404).json({ success: false, err: err });
+          } else {
+            let materialDict = {};
+            for (let i = 0; i < rows.length; i++) {
+              materialDict[rows[i].material_name] = rows[i].material_id;
             }
+            res.status(200).json({ success: true, materialDict: materialDict });
           }
-        );
-      } else {
-        res.status(409).json({ success: false, err: "使用者不存在" });
-      }
+        }
+      );
     }
-  );
+  });
 });
 
 router.post("/addNewMaterial", function (req, res, next) {
@@ -168,53 +152,61 @@ router.post("/addNewMaterial", function (req, res, next) {
     user_id: userId,
   };
   // insert new material
-  mysqlPoolQuery(
-    "SELECT material_name FROM material WHERE material_name = ?",
-    materialName,
-    function (err, rows) {
-      if (rows.length > 0) {
-        res.status(409).json({ success: false, err: "原料名稱已存在" });
-      } else {
-        mysqlPoolQuery(
-          "INSERT INTO material SET ?",
-          insertMaterial,
-          function (err, rows) {
-            if (err) {
-              res.status(404).json({ success: false, err: err });
-            } else if (materialAmount > 0) {
-              // 若有初始數量，則新增原料記錄
-              let insertMaterialHistory = {
-                mh_id: uuidv4(),
-                user_id: userId,
-                material_id: materialId,
-                amount: materialAmount,
-                price: materialPrice,
-                cost: materialAmount * materialPrice,
-                time: new Date(Date.now()),
-              };
-              mysqlPoolQuery(
-                "INSERT INTO material_history SET ?",
-                insertMaterialHistory,
-                function (err, rows) {
-                  if (err) {
-                    res.status(404).json({ success: false, err: err });
-                  } else {
-                    res
-                      .status(201)
-                      .json({ success: true, message: "新增初始原料成功" });
-                  }
+  checkUserId(userId, function (err, result) {
+    if (err) {
+      res.status(404).json({ success: false, err: err });
+    } else if (result == false) {
+      res.status(409).json({ success: false, err: "使用者不存在" });
+    } else {
+      mysqlPoolQuery(
+        "SELECT material_name FROM material WHERE material_name = ?",
+        materialName,
+        function (err, rows) {
+          if (rows.length > 0) {
+            res.status(409).json({ success: false, err: "原料名稱已存在" });
+          } else {
+            mysqlPoolQuery(
+              "INSERT INTO material SET ?",
+              insertMaterial,
+              function (err, rows) {
+                if (err) {
+                  res.status(404).json({ success: false, err: err });
+                } else if (materialAmount > 0) {
+                  // 若有初始數量，則新增原料記錄
+                  let insertMaterialHistory = {
+                    mh_id: uuidv4(),
+                    user_id: userId,
+                    material_id: materialId,
+                    amount: materialAmount,
+                    price: materialPrice,
+                    cost: materialAmount * materialPrice,
+                    time: new Date(Date.now()),
+                  };
+                  mysqlPoolQuery(
+                    "INSERT INTO material_history SET ?",
+                    insertMaterialHistory,
+                    function (err, rows) {
+                      if (err) {
+                        res.status(404).json({ success: false, err: err });
+                      } else {
+                        res
+                          .status(201)
+                          .json({ success: true, message: "新增初始原料成功" });
+                      }
+                    }
+                  );
+                } else {
+                  res
+                    .status(201)
+                    .json({ success: true, message: "新增初始原料成功" });
                 }
-              );
-            } else {
-              res
-                .status(201)
-                .json({ success: true, message: "新增初始原料成功" });
-            }
+              }
+            );
           }
-        );
-      }
+        }
+      );
     }
-  );
+  });
 });
 
 router.post("/deleteMaterial", function (req, res, next) {
@@ -227,17 +219,25 @@ router.post("/deleteMaterial", function (req, res, next) {
   const mysqlPoolQuery = req.pool;
   const userId = req.body.userId;
   const materialId = req.body.materialId;
-  mysqlPoolQuery(
-    "DELETE FROM material WHERE material_id = ? AND user_id = ?",
-    [materialId, userId],
-    function (err, rows) {
-      if (err) {
-        res.status(404).json({ success: false, err: err });
-      } else {
-        res.status(200).json({ success: true, message: "刪除原料成功" });
-      }
+  checkUserId(userId, function (err, result) {
+    if (err) {
+      res.status(404).json({ success: false, err: err });
+    } else if (result == false) {
+      res.status(409).json({ success: false, err: "使用者不存在" });
+    } else {
+      mysqlPoolQuery(
+        "DELETE FROM material WHERE material_id = ? AND user_id = ?",
+        [materialId, userId],
+        function (err, rows) {
+          if (err) {
+            res.status(404).json({ success: false, err: err });
+          } else {
+            res.status(200).json({ success: true, message: "刪除原料成功" });
+          }
+        }
+      );
     }
-  );
+  });
 });
 
 router.post("/updateAmount", function (req, res, next) {
@@ -264,53 +264,45 @@ router.post("/updateAmount", function (req, res, next) {
     cost: amountChange * price,
     time: new Date(Date.now()),
   };
-  mysqlPoolQuery(
-    "SELECT * FROM user WHERE user_id = ?",
-    userId,
-    function (err, rows) {
-      if (err) {
-        res.status(404).json({ success: false, err: err });
-      } else if (rows.length > 0) {
-        mysqlPoolQuery(
-          "SELECT * FROM material WHERE material_id = ?",
-          materialId,
-          function (err, rows) {
-            if (err) {
-              res.status(404).json({ success: false, err: err });
-            } else if (rows.length > 0) {
-              mysqlPoolQuery(
-                "UPDATE material SET material_amount = material_amount + ? WHERE material_id = ? AND user_id = ?",
-                [amountChange, materialId, userId],
-                function (err, rows) {
-                  if (err) {
-                    res.status(404).json({ success: false, err: err });
-                  } else {
-                    mysqlPoolQuery(
-                      "INSERT INTO material_history SET ?",
-                      insertMaterialHistory,
-                      function (err, rows) {
-                        if (err) {
-                          res.status(404).json({ success: false, err: err });
-                        } else {
-                          res
-                            .status(201)
-                            .json({ success: true, message: "更新原料成功" });
-                        }
-                      }
-                    );
+  checkUserId(userId, function (err, result) {
+    if (err) {
+      res.status(404).json({ success: false, err: err });
+    } else if (result == false) {
+      res.status(409).json({ success: false, err: "使用者不存在" });
+    } else {
+      checkMaterialId(materialId, function (err, result) {
+        if (err) {
+          res.status(404).json({ success: false, err: err });
+        } else if (result == false) {
+          res.status(409).json({ success: false, err: "原料不存在" });
+        } else {
+          mysqlPoolQuery(
+            "UPDATE material SET material_amount = material_amount + ? WHERE material_id = ? AND user_id = ?",
+            [amountChange, materialId, userId],
+            function (err, rows) {
+              if (err) {
+                res.status(404).json({ success: false, err: err });
+              } else {
+                mysqlPoolQuery(
+                  "INSERT INTO material_history SET ?",
+                  insertMaterialHistory,
+                  function (err, rows) {
+                    if (err) {
+                      res.status(404).json({ success: false, err: err });
+                    } else {
+                      res
+                        .status(201)
+                        .json({ success: true, message: "更新原料成功" });
+                    }
                   }
-                }
-              );
-            } else {
-              res.status(409).json({ success: false, err: "原料不存在" });
+                );
+              }
             }
-          }
-        );
-      } else {
-        res.status(409).json({ success: false, err: "使用者不存在" });
-      }
+          );
+        }
+      });
     }
-  );
+  });
 });
 
 module.exports = router;
---
+++ DBMS_project/backend/src/checkExisted.js
@@ -0,0 +1,35 @@
+const mysqlPoolQuery = require("./mysql");
+
+const checkUserId = (userId, callback) => {
+  mysqlPoolQuery(
+    "SELECT * FROM user WHERE user_id = ?",
+    userId,
+    function (err, rows) {
+      if (err) {
+        callback(err, null);
+      } else if (rows.length == 0) {
+        callback(null, false);
+      } else {
+        callback(null, true);
+      }
+    }
+  );
+};
+
+const checkMaterialId = (materialId, callback) => {
+  mysqlPoolQuery(
+    "SELECT * FROM material WHERE material_id = ?",
+    materialId,
+    function (err, rows) {
+      if (err) {
+        callback(err, null);
+      } else if (rows.length == 0) {
+        callback(null, false);
+      } else {
+        callback(null, true);
+      }
+    }
+  );
+};
+
+module.exports = { checkUserId, checkMaterialId };
