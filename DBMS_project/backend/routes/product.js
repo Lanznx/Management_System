7a5771e5e41327bfe514eb9f70641103a8ceba12
grace0950 <pinyan0950@gmail.com>
2022-05-15 20:30:52 +0800

    [product] -> add new api: getAllProduct

--- DBMS_project/backend/routes/product.js
+++ DBMS_project/backend/routes/product.js
@@ -8,7 +8,46 @@ router.get("/", function (req, res, next) {
 });
 
 router.post("/getAllProducts", function (req, res, next) {
-  // #swagger.tags = ['Unfinished']
+  /*
+   #swagger.tags = ['Product']
+   #swagger.responses[409] = {
+    description: '使用者或商品不存在'
+   }
+  */
+  const mysqlPoolQuery = req.pool;
+  const userId = req.body.userId;
+  mysqlPoolQuery(
+    "SELECT * FROM user WHERE user_id = ?",
+    userId,
+    function (err, rows) {
+      if (err) {
+        res.status(404).json({ success: false, err: err });
+      } else if (rows.length == 0) {
+        res.status(409).json({ success: false, err: "使用者不存在" });
+      } else {
+        // res.status(200).json({ success: true, userInformation: rows[0] });
+        mysqlPoolQuery(
+          "SELECT product_id AS productId, product_name AS name, product_price AS price, product_amount AS amount FROM product WHERE user_id = ?",
+          userId,
+          function (err, rows) {
+            if (err) {
+              res.status(404).json({ success: false, err: err });
+            } else {
+              if (rows.length == 0) {
+                //如果有user沒資料
+                res.status(409).json({ success: false, err: "尚無商品" });
+              } else {
+                //剩下 回傳json格式
+                res
+                  .status(200)
+                  .json({ success: true, allUserInformation: rows });
+              }
+            }
+          }
+        );
+      }
+    }
+  );
 });
 
 router.post("/getProduct", function (req, res, next) {
