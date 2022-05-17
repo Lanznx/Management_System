7cba07be02be4cb57c71778367e5136da5aa31d8
Brandon <109305092@g.nccu.edu.tw>
2022-05-18 00:09:07 +0800

    feat: order -> add OrderTable, fix render bug

--- DBMS_project/webui/src/System/Order/index.js
+++ DBMS_project/webui/src/System/Order/index.js
@@ -1,6 +1,6 @@
-import { Box } from "@mui/system";
-import { Container, Grid } from "@mui/material";
-import { ProductCard } from "./components/ProductCard.js";
+import { Grid } from "@mui/material";
+import ProductCard from "./components/ProductCard.js";
+import OrderTable from "./components/OrderTable";
 import { useEffect, useState } from "react";
 import SendIcon from "@mui/icons-material/Send";
 import IconButton from "@mui/material/IconButton";
@@ -9,6 +9,23 @@ const axios = require("axios");
 export default function Order() {
   const [productInfos, setProductInfos] = useState([]);
   const [canSend, setCanSend] = useState(false);
+  const [orders, setOrders] = useState([]);
+  const [toZero, setToZero] = useState(false);
+  function orderRec(name, productId, price, amount) {
+    console.log("first");
+    let order = {
+      name: name,
+      productId: productId,
+      price: price,
+      amount: amount,
+    };
+    const newOrders = [
+      ...orders.filter((item) => item.productId !== productId),
+      order,
+    ];
+    setOrders(newOrders);
+  }
+
   useEffect(() => {
     axios({
       method: "post",
@@ -23,29 +40,39 @@ export default function Order() {
   }, []);
   return (
     <Grid container spacing={2} sx={{ display: "flex" }}>
-      {productInfos.map((productInfo) => (
-        <Grid key={productInfo.productId} item md={6}>
-          <ProductCard
-            productInfo={productInfo}
-            sx={{ justifyContent: "flex-end" }}
-          />
-        </Grid>
-      ))}
+      <Grid container md={6}>
+        {productInfos.map((productInfo) => (
+          <Grid item md={6} key={productInfo.productId}>
+            <ProductCard
+              toZero={toZero}
+              setToZero={setToZero}
+              orderRec={orderRec}
+              productInfo={productInfo}
+              sx={{ justifyContent: "flex-end", width: "30px" }}
+            />
+          </Grid>
+        ))}
+      </Grid>
+
       <Grid item md={6}>
-        <IconButton
-          sx={{ marginLeft: "24px", color: canSend ? "#0095E6" : "grey", "&:hover":{color: '#C4090E'} }}
-          onClick={()=>{
-            // setProductInfos(productInfos.map((productInfo)=>{productInfo.amount = 0}))
-            
-          }}
-        >
-          <CancelIcon />
-        </IconButton>
-        <IconButton
-          sx={{ marginLeft: "24px", color: canSend ? "#0095E6" : "grey" }}
-        >
-          <SendIcon />
-        </IconButton>
+        <OrderTable orders={orders} />
+        <Grid item md={12}>
+          <IconButton
+            sx={{
+              color: canSend ? "#0095E6" : "grey",
+              "&:hover": { color: "#C4090E" },
+            }}
+            onClick={() => {
+              setOrders(orders.filter((item) => item.productId === 0));
+              setToZero(true);
+            }}
+          >
+            <CancelIcon />
+          </IconButton>
+          <IconButton sx={{ color: canSend ? "#0095E6" : "grey" }}>
+            <SendIcon />
+          </IconButton>
+        </Grid>
       </Grid>
     </Grid>
   );
