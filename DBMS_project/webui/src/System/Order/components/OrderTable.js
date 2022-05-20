6dd302be1134826b5d4dd3073326d74a41353f71
Brandon <109305092@g.nccu.edu.tw>
2022-05-21 01:24:50 +0800

    feat: OrderPage  版面完成, WIP: 串接API

--- DBMS_project/webui/src/System/Order/components/OrderTable.js
+++ DBMS_project/webui/src/System/Order/components/OrderTable.js
@@ -8,15 +8,42 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
 export default function OrderTable(props) {
+  const [totalAmount, setTotalAmount] = React.useState(0);
+  const [totalPrice, setTotalPrice] = React.useState(0);
+
+  React.useEffect(() => {
+    let subTotalAmount = 0;
+    let subTotalPrice = 0;
+    props.orders.map((order) => {
+      console.log(order);
+      subTotalAmount += order.amount;
+      subTotalPrice += order.price * order.amount;
+      setTotalAmount(subTotalAmount);
+      setTotalPrice(subTotalPrice);
+    });
+  }, [props.orders]);
+
+  React.useEffect(() => {
+    setTotalAmount(0);
+    setTotalPrice(0);
+  }, [props.toZero]);
 
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} sx={{ marginTop: "12px" }}>
       <Table sx={{ minWidth: 400 }} aria-label="simple table">
-        <TableHead>
+        <TableHead sx={{ backgroundColor: "#1976d2" }}>
           <TableRow>
-            <TableCell>品項</TableCell>
-            <TableCell align="right">價錢</TableCell>
-            <TableCell align="right">數量</TableCell>
+            <TableCell sx={{ color: "#fff", fontWeight: "bold" }}>
+              品項
+            </TableCell>
+
+            <TableCell sx={{ color: "#fff", fontWeight: "bold" }} align="right">
+              數量
+            </TableCell>
+
+            <TableCell sx={{ color: "#fff", fontWeight: "bold" }} align="right">
+              價錢
+            </TableCell>
           </TableRow>
         </TableHead>
 
@@ -29,10 +56,22 @@ export default function OrderTable(props) {
               <TableCell component="th" scope="row">
                 {order.name}
               </TableCell>
-              <TableCell align="right">{order.price}</TableCell>
               <TableCell align="right">{order.amount}</TableCell>
+              <TableCell align="right">{order.price}</TableCell>
+
             </TableRow>
           ))}
+          <TableRow>
+            <TableCell align="left" sx={{ fontWeight: "bold" }}>
+              加總
+            </TableCell>
+            <TableCell align="right"  sx={{ fontWeight: "bold" }}>
+              {totalAmount}
+            </TableCell>
+            <TableCell align="right" sx={{ fontWeight: "bold" }}>
+              {totalPrice}
+            </TableCell>
+          </TableRow>
         </TableBody>
       </Table>
     </TableContainer>
