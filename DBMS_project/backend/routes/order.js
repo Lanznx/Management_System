688098446196243c430e4066bc112c46eda8b03c
grace0950 <pinyan0950@gmail.com>
2022-06-01 17:58:02 +0800

    [order] -> add new api: addNewTag

--- DBMS_project/backend/routes/order.js
+++ DBMS_project/backend/routes/order.js
@@ -45,8 +45,43 @@ router.post("/getTagDict", async function (req, res, next) {
 
 router.post("/addNewTag", async function (req, res, next) {
   /*
-  #swagger.tags = ['Unfinished']
+  #swagger.tags = ['Order']
+  #swagger.responses[409] = {
+    description: '使用者不存在或標籤已存在'
+  }
+  #swagger.responses[200] = {
+    description: '取得成功',
+  }
   */
+  const mysqlPoolQuery = req.pool;
+  const userId = req.body.userId;
+  const tagName = req.body.tagName;
+  const tagId = uuidv4();
+  let insertTag = {
+    tag_id: tagId,
+    tag_name: tagName,
+    user_id: userId,
+  };
+  try {
+    const userExisted = await checkUserId(userId);
+    if (!userExisted) {
+      res.status(409).json({ success: false, err: "使用者不存在" });
+    } else {
+      // insert new tag
+      let rows = await mysqlPoolQuery(
+        "SELECT tag_name FROM tag WHERE tag_name = ? AND user_id = ?",
+        [tagName, userId]
+      );
+      if (rows.length > 0) {
+        res.status(409).json({ success: false, err: "標籤名稱已存在" });
+      } else {
+        await mysqlPoolQuery("INSERT INTO tag SET ?", insertTag);
+        res.status(201).json({ success: true, message: "新增標籤成功" });
+      }
+    }
+  } catch (err) {
+    res.status(404).json({ success: false, err: err });
+  }
 });
 
 router.post("/addNewOrder", async function (req, res, next) {
