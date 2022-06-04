cead7e1f2b58c329037a7f1308e32198cc172a06
Brandon <109305092@g.nccu.edu.tw>
2022-06-05 00:28:01 +0800

    feat: 德彥的屁股

---
+++ DBMS_project/webui/src/System/Order/OrderList/index.js
@@ -0,0 +1,13 @@
+import { Grid, Typography } from "@mui/material";
+
+export default function OrderList() {
+
+    return (
+      <Grid container spacing={2} sx={{ display: "flex" }}>
+        <Typography variant="h5" component="div">
+            這邊是 OrderList !!!
+        </Typography>
+      </Grid>
+    );
+  }
+  
