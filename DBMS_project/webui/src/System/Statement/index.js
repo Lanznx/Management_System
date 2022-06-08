4158a5a0242399bcbdbf54be1282fa62ad7cc648
YJack0000 <jackleagends@gmail.com>
2022-06-09 01:13:41 +0800

    輸出假檔案

--- DBMS_project/webui/src/System/Statement/index.js
+++ DBMS_project/webui/src/System/Statement/index.js
@@ -1,5 +1,27 @@
+import ExportCSV from "./ExportCSV";
+
 export default function Statement(){
+    const file = new File(["本月損益表"], "foo.xlsx", {
+        type: "text/plain",
+    });
+
+    function download() {
+        const link = document.createElement('a')
+        const url = URL.createObjectURL(file)
+      
+        link.href = url
+        link.download = file.name
+        document.body.appendChild(link)
+        link.click()
+      
+        document.body.removeChild(link)
+        window.URL.revokeObjectURL(url)
+    }
+
     return (
-        <div> statement </div>
+        <div>
+        {/* <ExportCSV /> */}
+        <button style={{opacity: '0'}} onclick={download()}>Click me</button>
+        </div>
     )
 }
